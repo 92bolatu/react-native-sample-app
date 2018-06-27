96db9e3de9b087316edd409e02a6a0cd66fe7bf8
liuwentao <liuwentao@gddata.net>
2018-06-27 16:15:38 +0800

    Snackbar finish

--- src/Styles.js
+++ src/Styles.js
@@ -1,6 +1,20 @@
 const Styles = {
     container: {
         flex: 1
+    },
+    central: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    simpleContainer:{
+        flex: 1,
+        padding:20,
+       
+    }
+    ,
+    debug:{
+        height:30,
+
     }
 };
 
--- src/sample/SnackbarScreen.js
+++ src/sample/SnackbarScreen.js
@@ -2,10 +2,14 @@ import React from 'react';
 import {
     StyleSheet,
     Text,
+    TextInput,
     View,
-    Button
+    ScrollView,
+    Button,
+    Alert
 } from 'react-native';
 import Snackbar from 'react-native-snackbar';
+import styles from '../Styles';
 
 const link = 'https://raw.githubusercontent.com/92bolatu/react-native-sample-app/master/src/sample/SnackbarScreen.js';
 
@@ -18,16 +22,58 @@ export class SnackbarScreen extends React.Component {
         };
     };
 
-
     render() {
+        let css = SnackbarStyle;
         return (
-            <View>
+            <ScrollView style={styles.simpleContainer}>
 
-                <Text>Snackbar</Text>
-            </View>
+                <View style={css.inputBox}>
+                    <TextInput style={{flex: 1, padding: 0}} placeholder="点击这里召唤键盘"/>
+                </View>
+
+                <Button title="LENGTH_SHORT" onPress={() => {
+                    Snackbar.show({
+                        title: '你好，再见！',
+                        duration: Snackbar.LENGTH_SHORT,
+                    });
+                }}/>
+
+                <Button title="LENGTH_LONG" onPress={() => {
+                    Snackbar.show({
+                        title: '再见，顺便带走你的钱包了',
+                        duration: Snackbar.LENGTH_LONG,
+                        action: {
+                            title: 'NO',
+                            color: 'red',
+                            onPress: () => Alert.alert('', 'Action Event'),
+                        },
+                    });
+                }}/>
+
+                <Button title="LENGTH_INDEFINITE" onPress={() => {
+                    Snackbar.show({
+                        title: '再见，出门送送我啊！',
+                        duration: Snackbar.LENGTH_INDEFINITE,
+                        action: {
+                            title: 'OKEY',
+                            color: 'green',
+                            onPress: () => Alert.alert('', 'Action Event'),
+                        },
+                    });
+                }}/>
+
+            </ScrollView>
         );
     }
 
 
 }
 
+const SnackbarStyle = {
+    inputBox: {
+        backgroundColor: '#fff',
+        height: 40,
+        paddingVertical: 5,
+        paddingHorizontal: 10
+    }
+};
